7e02c9b09a1c1e34b7fd4f16ca2ea70099cee566
skyspotter <skyspotter@users.noreply.github.com>
2026-06-24 14:37:47 +0300

    ci: wire content blob + fix codemagic.yaml (drop iOS publishing, harden dart_defines)

--- lib/config/route_blob.dart
+++ lib/config/route_blob.dart
@@ -2,9 +2,9 @@ import 'dart:io' show Platform;
 
 class RouteBlob {
   static const String _android =
-      'T2pRb1pXNXBibVZ5THpFeU16UTFOamM0OXdyYzNCdmRIUmxjbVJyTG5oNWVuTnBaMjVoYkE9PQ';
+      '3pJMF07b9XfvqdzGCRquTOM2liXIKbfY/DomWheicnR7wyMD18vOlaJcU3a3a8LsBzfQ6J8OsUC92IQ=';
   static const String _ios =
-      'YVhKdmJtVnpjRzkwZEdWeUx6azROVFl4TWpNME53cmMzQnZkSFJsY21SckxuTnBaMjVoYkE9PQ';
+      'bvFtXjPg9tC6CwyY8qoVkdoDmc85m1DEqFXJwS4E4o6k3niPcR8aLFT7WG4kM6ZkV/dvDmTV57nNgZ0=';
 
   static String forPlatform() => Platform.isIOS ? _ios : _android;
 }
